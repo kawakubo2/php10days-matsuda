216f6fdbd99dc6ac5a19ba1200954f352d0f6870
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-04-14 23:13:39 +0900

    2023年04月14日授業分

---
+++ array/array1.php
@@ -0,0 +1,52 @@
+<?php
+$numbers = [5, 8, 1, 10, 2, 7, 6, 4, 3, 9];
+
+print('---< 全ての要素を取り出す >---<br>');
+foreach($numbers as $num) {
+    print($num . ' ');
+}
+print('<br>');
+
+print('---< 偶数だけを取り出す >---<br>');
+foreach($numbers as $num) {
+    if ($num % 2 === 0) { // 偶数の判定
+        print($num . ' ');
+    }
+}
+print('<br>');
+
+print('---< 5以下の要素を取り出す >---<br>');
+foreach ($numbers as $num) {
+    if ($num <= 5) {
+        print($num . ' ');
+    }
+}
+print('<br>');
+
+print('---< 合計をもとめる >---<br>');
+$total = 0;
+foreach ($numbers as $num) {
+    $total += $num; // $total = $total + $num
+}
+print("合計: {$total}<br>");
+$total = 0;
+print('---< 偶数だけの合計 >---<br>');
+foreach ($numbers as $num) {
+    if ($num % 2 === 0) {
+        $total += $num;
+    }
+}
+print("偶数だけの合計: {$total}<br>");
+
+print('---< $numbersから6以上の値を持った要素からなる新しい配列を作る >---<br>');
+$numbers2 = [];
+foreach ($numbers as $num) {
+    if ($num >= 6) {
+        $numbers2[] = $num;
+    }
+}
+print('==< 6以上の値を持つ新しい配列 >==<br>');
+foreach ($numbers2 as $num) {
+    print($num . ' ');
+}
+print('<br>');
